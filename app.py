0212b7ebebbdd4f0b1f218ceabd2e6dad01ed685
Mritunjay Singh <mritunjay.1121@gmail.com>
2022-06-17 21:26:04 +0530

    Updated

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask
 
 app=Flask(__name__)
@@ -5,7 +6,7 @@ app=Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 
 def index():
-    return "Starting Machine Learning Project"
+    return "This is CI/CD Pipeline 1"
 
 if __name__=="__main__":
     app.run(debug=True)
