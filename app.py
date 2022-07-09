ae37ecafbbb44029b65a8fce72c1852bb187229b
Mritunjay Singh <mritunjay.1121@gmail.com>
2022-07-09 12:59:10 +0530

    updated

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def index():
         housing = HousingException(e,sys)
         logging.info(housing.error_message)
         logging.info("We are testing logging module")
-    return "Hey! this is CI/CD Pipeline 1"
+    return "Hey! this is CI/CD Pipeline"
 
 if __name__=="__main__":
     app.run(debug=True)
